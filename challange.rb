28e113b0d4e06686dbd83912174e1b9b82f045cc
Krishna <libraryismyparadise@gmail.com>
2022-11-11 19:17:02 +0545

    Added Buggy code

---
+++ challange.rb
@@ -0,0 +1,12 @@
+puts 'Pick an option: 1 or 2'
+user_input = gets.chomp
+
+if user_input == 1
+  puts 'You have selected 1'
+elsif user_input == 2
+  puts 'You have selected 2'
+  
+else
+  puts 'Something Else that I failed to Recognise'
+  
+end
